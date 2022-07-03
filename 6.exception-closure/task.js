7edaa85999238ec9423a7f838e96fbb23112cb61
Юлия Смирнова <dolphin_99@mail.ru>
2022-07-03 21:42:18 +0300

    Not fully completed task 2.0

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,7 +18,7 @@ function validateCount(string) {
 
 class Triangle {
   constructor(side1, side2, side3) {
-    if ((side1 < side2 + side3) || (side2 < side1 + side3) || (side3 < side1 + side2)) {
+    if ((side1 > side2 + side3) || (side2 > side1 + side3) || (side3 > side1 + side2)) {
       throw new Error("Треугольник с такими сторонами не существует");
     } else {
       this.side1 = side1;
@@ -32,8 +32,8 @@ class Triangle {
   }
 
   getArea() {
-    p = ((this.side1 + this.side2 + this.side3) / 2);
-    return this.area = (Math.sqrt(semi_periment(p - this.side1)(p - this.side2)(p - this.side3))).toFixed(3);
+    let p = ((this.side1 + this.side2 + this.side3) / 2);
+    return this.area = (Math.sqrt(p * (p - this.side1)(p - this.side2)(p - this.side3))).toFixed(3);
   }
 }
 
@@ -41,6 +41,9 @@ function getTriangle(side1, side2, side3) {
   try {
     return new Triangle(side1, side2, side3)
   } catch (error) {
-    return Triangle.getArea(error) && Triangle.getPerimeter(error);    
+    let theObject = new Object();
+    theObject.getArea = () => 'Ошибка! Треугольник не существует';
+    theObject.getPerimeter = () => 'Ошибка! Треугольник не существует';
+    return theObject;    
   }
 }
