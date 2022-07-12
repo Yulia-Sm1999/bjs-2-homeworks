0c6f388d75fdb0b9d64edb924d95761c1c426bca
Юлия Смирнова <dolphin_99@mail.ru>
2022-07-12 21:51:19 +0300

    Completed task???

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,47 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    
+    cache.push({ hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = 0;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function (...args) {
+    timeoutId += 1;
+    if (timeoutId > 1) {
+      setTimeout(() => (func(...args)), delay);
+    }
+    func(...args)
+  }
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timeoutId = 0;
+  wrapper.count = 0;
+
+   function wrapper(...args) {
+    timeoutId += 1;
+    wrapper.count += 1;
+    if (timeoutId > 1) {
+      setTimeout(() => (func(...args)), delay);
+    }
+    func(...args)
+  }
+  return wrapper;
 }
